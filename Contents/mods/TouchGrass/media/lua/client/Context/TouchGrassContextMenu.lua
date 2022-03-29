a034dea0343537e8d2c289f30415cb051c2058b5
Preston <78995156+LiminalCrab@users.noreply.github.com>
2022-03-29 19:58:53 -0400

    Update TouchGrassContextMenu.lua

--- Contents/mods/TouchGrass/media/lua/client/Context/TouchGrassContextMenu.lua
+++ Contents/mods/TouchGrass/media/lua/client/Context/TouchGrassContextMenu.lua
@@ -2,6 +2,16 @@
 TouchGrass = {}
 require "ISUI\ISWorldObjectContextMenu.lua"
 
+TouchGrass.createMenu = function(player, worldobjects, x, y, test)
+    local playerObj = getSpecificPlayer(player)
+    local context = ISContextMenu.get(player, x, y);
+
+    if not playerObj:getVehicle() then
+        if test == true then return true; end
+        context:addOption("Touch grass ", worldobjects, TouchGrass.onTouchGrass, ISWorldObjectContextMenu.canBeRemoved, player);
+    end
+end
+
 
 TouchGrass.onTouchGrass = function(worldobjects, square, player)
     local playerObj = getSpecificPlayer(player)
@@ -15,3 +25,4 @@ TouchGrass.doTouchGrass = function(playerObj, square)
     end
 end
 
+Events.OnFillWorldObjectContextMenu.Add(TouchGrass.createMenu)
