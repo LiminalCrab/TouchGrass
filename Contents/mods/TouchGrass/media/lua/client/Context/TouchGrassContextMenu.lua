0f71d58b0383b50fd43691d18d473fd78b621702
Preston <78995156+LiminalCrab@users.noreply.github.com>
2022-03-29 20:09:41 -0400

    Update TouchGrassContextMenu.lua

--- Contents/mods/TouchGrass/media/lua/client/Context/TouchGrassContextMenu.lua
+++ Contents/mods/TouchGrass/media/lua/client/Context/TouchGrassContextMenu.lua
@@ -3,25 +3,27 @@ TouchGrass = {}
 require "ISUI\ISWorldObjectContextMenu.lua"
 
 TouchGrass.createMenu = function(player, worldobjects, x, y, test)
-    local playerObj = getSpecificPlayer(player)
+    if test and ISWorldObjectContextMenu.Test then return true end
+
+    local playerObj = getPlayer(0)
     local context = ISContextMenu.get(player, x, y);
 
     if not playerObj:getVehicle() then
         if test == true then return true; end
-        context:addOption("Touch grass ", worldobjects, TouchGrass.onTouchGrass, ISWorldObjectContextMenu.canBeRemoved, player);
+        context:addOption("Touch grass ", worldobjects, TouchGrass.onTouchGrass, canBeRemoved, player);
     end
 end
 
 
 TouchGrass.onTouchGrass = function(worldobjects, square, player)
-    local playerObj = getSpecificPlayer(player)
+    local playerObj = getPlayer(0)
     local bo = TouchGrassCursor:new(playerObj, "grass") --what
     getCell():setDrag(bo, player);
 end
 
 TouchGrass.doTouchGrass = function(playerObj, square)
     if luautils.walkAdj(playerObj, square, true) then
-        ISTimedActionQueue.add(TouchGrassTimedAction:new(playerObj, square))
+        ISTimedActionQueue.add(TouchGrassAction:new(playerObj, square))
     end
 end
 
